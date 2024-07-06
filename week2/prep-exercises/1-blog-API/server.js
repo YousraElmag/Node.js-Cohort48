360a7ec60af1b63bb8604f5f14eab809bf979e30
YousraElmag <yousra3489@gmail.com>
2024-07-06 16:08:23 +0200

    done

--- week2/prep-exercises/1-blog-API/server.js
+++ week2/prep-exercises/1-blog-API/server.js
@@ -1,10 +1,103 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
 const app = express();
- 
+app.use(express.json());
+const PORT = 3000;
 
-// YOUR CODE GOES IN HERE
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.post('/blogs', (req, res) => {
+    const { title, content } = req.body;
+    if (!title || !content) {
+        return res.status(400).send('Title and content are required');
+    }
  
-app.listen(3000)
+    let blogs = [];
+    
+        const data = fs.readFileSync('blogs.json', 'utf-8');
+        blogs = JSON.parse(data);
+   
+    
+   
+    blogs.push({ title, content });
+    
+    
+    fs.writeFileSync('blogs.json', JSON.stringify(blogs));
+    res.send('ok');
+});
+
+app.get('/blogs/:title', (req, res) => {
+    const title = req.params.title;
+    
+  
+    let blogs = [];
+    try {
+        const data = fs.readFileSync('blogs.json', 'utf-8');
+        blogs = JSON.parse(data);
+    } catch (error) {
+        return res.status(404).send('No blogs found');
+    }
+    
+  
+    const blog = blogs.find(el => el.title === title);
+    if (!blog) {
+        return res.status(404).send('Blog not found');
+    }
+    
+    res.send(blog);
+});
+
+app.put('/blogs/:title', (req, res) => {
+    const title = req.params.title;
+    const { content } = req.body;
+
+    if (!content) {
+        return res.status(400).send('Content is required');
+    }
+    
+   
+    let blogs = [];
+    try {
+        const data = fs.readFileSync('blogs.json', 'utf-8');
+        blogs = JSON.parse(data);
+    } catch (error) {
+        return res.status(404).send('No blogs found');
+    }
+    
+    
+    const blogIndex = blogs.findIndex(el => el.title === title);
+    if (blogIndex === -1) {
+        return res.status(404).send('Blog not found');
+    }
+
+    blogs[blogIndex].content = content;
+
+
+    fs.writeFileSync('blogs.json', JSON.stringify(blogs));
+    res.send('Blog updated');
+});
+app.delete('/blogs/:title', (req, res) => {
+    const title = req.params.title;
+    
+ 
+    let blogs = [];
+    try {
+        const data = fs.readFileSync('blogs.json', 'utf-8');
+        blogs = JSON.parse(data);
+    } catch (error) {
+        return res.status(404).send('No blogs found');
+    }
+ 
+    const blogIndex = blogs.findIndex(el => el.title === title);
+    if (blogIndex === -1) {
+        return res.status(404).send('Blog not found');
+    }
+
+    blogs.splice(blogIndex, 1);
+
+  
+    fs.writeFileSync('blogs.json', JSON.stringify(blogs));
+    res.send('Blog deleted');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
