9a9a4d4c6656e17b1bc5ec6aea35c73778ee4f4f
YousraElmag <yousra3489@gmail.com>
2024-07-13 14:37:53 +0200

    done

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,15 +1,19 @@
 import express from 'express';
 // TODO Use below import statement for importing middlewares from users.js for your routes
 // TODO import { ....... } from "./users.js";
-
+import bodyParser from 'body-parser';
 let app = express();
-
+app.use(bodyParser.json());
 app.use(express.json());
 // TODO: Create routes here, e.g. app.post("/register", .......)
-
+import { login, profile, register ,logout} from './users.js';
 // Serve the front-end application from the `client` folder
 app.use(express.static('client'));
+app.post('/register', register)
+app.post('/login',login)
+app.get('/profile',profile)
+app.post('/logout',logout)
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000')
 });
--- week3/prep-exercise/server/users.js
+++ week3/prep-exercise/server/users.js
@@ -1,5 +1,110 @@
-// Create middlewares required for routes defined in app.js
-// export const register = async (req, res) => {};
 
-// You can also create helper functions in this file to help you implement logic
-// inside middlewares
+import bcrypt from 'bcryptjs' ;
+import fs from 'fs'
+import jwt from 'jsonwebtoken'
+import { v4 as uuidv4 } from 'uuid';
+
+let users = {};
+
+
+const dataFilePath = './data.json';
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+      if (err) {
+          return res.status(401).json({ message: 'Unauthorized' });
+      }
+      req.user = user;
+      next();
+  });
+};
+if (fs.existsSync(dataFilePath)) {
+    const data = fs.readFileSync(dataFilePath, 'utf-8');
+    if (data) {
+        users = JSON.parse(data);
+    }
+}
+// register
+ export const register= async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ message: "Username and password are required" });
+        }
+
+        
+        if (users[username]) {
+            return res.status(409).json({ message: "Username already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const userId = uuidv4();
+        users[username] = {
+            id: userId,
+            username: username,
+            password: hashedPassword
+        };
+        fs.writeFileSync(dataFilePath, JSON.stringify(users, null, 2));
+        res.status(201).json({ id: userId, username: username });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const SECRET_KEY = '123';
+export const login = async (req, res) => {
+  try {
+      const { username, password } = req.body;
+
+      if (!username || !password) {
+          return res.status(400).json({ message: "Username and password are required" });
+      }
+      const user = users[username];
+      if (!user) {
+          return res.status(401).json({ message: "Invalid username or password" });
+      }
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+          return res.status(401).json({ message: "Invalid username or password" });
+      }
+      const token = jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
+
+      res.status(201).json({ token: token });
+
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+}
+
+export const profile= (req, res) => {
+  try {
+  
+      const user = users[req.user.username];
+      if (!user) {
+          return res.status(401).json({ message: 'Unauthorized' });
+      }
+
+   
+      res.status(200).json({ username: user.username });
+
+  } catch (error) {
+      res.status(500).json({ message: error.message });
+  }
+}
+export const logout = (req, res) => {
+  res.status(204).send();
+};
+ 
+      
+
+
